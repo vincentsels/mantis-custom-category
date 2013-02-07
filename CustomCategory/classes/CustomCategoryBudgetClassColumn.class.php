da5823f11a2bf4867be1f71d27a5ea916a571485
Vincent Sels <vincent_sels@hotmail.com>
2013-02-07 14:44:19 +0100

    Changed logic

--- CustomCategory/classes/CustomCategoryBudgetClassColumn.class.php
+++ CustomCategory/classes/CustomCategoryBudgetClassColumn.class.php
@@ -21,6 +21,9 @@ class CustomCategoryBudgetClassColumn extends MantisColumn {
             project_hierarchy_get_parent( $p_bug->project_id ) == 307 ) {
             echo 'Werking Certia';
         }
+        else if ( $p_bug->project_id == 237 ) { # Regressietest
+            echo 'Correctief onderhoud';
+        }
         else if ( $p_bug->project_id == 248 || # Service Desk
             $p_bug->project_id == 294 ) { # CB
             ## Non-roadmap project
